10b6f3c8de3810f9b09ecc503ba3cf5749e7a056
Mokkaq <gbugajska051@gmail.com>
2024-11-16 20:56:23 +0100

    commit 2

--- testyGoogle/test.cpp
+++ testyGoogle/test.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include <cstdlib>
 
-// Zachowuje tablicê niezmienion¹, gdy ona jest ju¿ posortowana rosn¹co
+// Zachowuje tablicê niezmienion¹, gdy ona jest ju¿ posortowana rosn¹co    
 TEST(Sort, TabNiezmienionaRosnaco) {
 	int tab[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int size = sizeof(tab) / sizeof(tab[0]);
